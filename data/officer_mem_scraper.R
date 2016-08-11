e95b5c8d7ab429fd9286c8349cca74ed424f9c45
Mike Bales <mike.bales@gmail.com>
2016-08-11 13:51:57 -0400

    copied changes from for-loop-broken

--- data/officer_mem_scraper.R
+++ data/officer_mem_scraper.R
@@ -7,12 +7,12 @@ href <- c()
 
 for (i in seq(0,450,25)) { 
   
-tmp <- paste0('https://www.odmp.org/search?from=2013&to=2016&filter=nok9&o=',i) %>%
-       read_html() %>%
-       html_nodes('#secondary-layout-one-column-body td+ td a') %>%
-       html_attr('href')
+  tmp <- paste0('https://www.odmp.org/search?from=2013&to=2016&filter=nok9&o=',i) %>%
+    read_html() %>%
+    html_nodes('#secondary-layout-one-column-body td+ td a') %>%
+    html_attr('href')
   
-href <- c(href, tmp)
+  href <- c(href, tmp)
 }
 
 ## Get "Bio & Incident Details" from officer memorial page ##
@@ -24,28 +24,32 @@ memsplit <- function(x) {
 
 all_bio <- list()
 for (k in 1:length(href)) {
-odmp <- read_html(href[k])
-bio <- data.frame(rank = memsplit(2), 
-         name = memsplit(3),       
-         department = memsplit(4),
-         death.date = str_trim(strsplit(strsplit(memsplit(5), '\t\t\t\t\t')[[1]][1],':')[[1]][2]), stringsAsFactors = FALSE)
-
-i = 0 
-j = length(bio) + 1
-repeat {
-      i = i + 1
+  odmp <- read_html(href[k])
+  bio <- data.frame(rank = memsplit(2), 
+                    name = memsplit(3),       
+                    department = memsplit(4),
+                    death.date = str_trim(strsplit(strsplit(memsplit(5), '\t\t\t\t\t')[[1]][1],':')[[1]][2]),
+                    summary = strsplit((odmp %>% html_node('#memorial_featuredBody_right p:nth-child(1)') %>% html_text()),'\n')[[1]][1],
+                    stringsAsFactors = FALSE
+  )
   
-      node <- html_node(odmp, paste0("p:nth-child(", i+1,")"))
-      if (is.na(html_text(node))) {break}
-      varPair <- strsplit(html_text(node),":")
-      if (!is.na(varPair[[1]][2])) {
-       bio[j] <- str_trim(varPair[[1]][2])
-       names(bio)[j] <- varPair[[1]][1]
-       j = j + 1 
-      }
-     } 
-    all_bio[k] <- list(bio)
+  
+  i = 0 
+  j = length(bio) + 1
+  repeat {
+    i = i + 1
+    
+    node <- html_node(odmp, paste0("p:nth-child(", i+1,")"))
+    if (is.na(html_text(node))) {break}
+    varPair <- strsplit(html_text(node),":")
+    if (!is.na(varPair[[1]][2])) {
+      bio[j] <- str_trim(varPair[[1]][2])
+      names(bio)[j] <- varPair[[1]][1]
+      j = j + 1 
     }
+  } 
+  all_bio[k] <- list(bio)
+}
 
 leo.df <- Reduce(function(...) merge(..., all=TRUE), all_bio)
 
--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var data,
-    margin = { top: 15, right: 15, bottom: 40, left: 15 },
+    margin = { top: 10, right: 15, bottom: 30, left: 15 },
     cellSize = 3,
     width = (cellSize*365+365) - margin.left - margin.right,
-    height = 100 - margin.top - margin.bottom;
+    height = 85 - margin.top - margin.bottom
+    years = [2013, 2014, 2015];
 
 //DATE PARSER
 
@@ -19,32 +20,24 @@ var dateParser = d3.timeParse("%Y-%m-%d"),
 d3.queue()
   .defer(d3.json, 'data/polviol.json')
   .awaitAll(function (error, results) {
-    if (error) { throw error; }
+    if (error) { throw error; };
     data = results[0];
 
-    var chart1 = new Chart(2013),
-        chart2 = new Chart(2014),
-        chart3 = new Chart(2015);
+    var chart1 = new Chart();
+        //chart2 = new Chart(2014),
+        //chart3 = new Chart(2015);
   });
 
 
-function Chart(year) {
+function Chart() {
+
+
     var chart = this;
 
-    txData = data.slice();
+    // VICTIM DETAILS DIV
 
-    txData = txData.filter(function (d) {
-        return yearFormat(dateParser(d.deathDate)) === year.toString();
-      }); 
-
-
-
-    // Tooltips from http://bl.ocks.org/d3noob/a22c42db65eb00d4e369
-    // Define the div for the tooltip
-
-    var div = d3.select("body").append("div") 
-                .attr("class", "tooltip")       
-                .style("opacity", 0);
+    var div = d3.select(".details")
+                .selectAll("p");
 
 
     // SVG
@@ -52,55 +45,74 @@ function Chart(year) {
     chart.svg = d3.select('#chart')
                   .append('svg')
                   .attr("width", width + margin.right + margin.left)
-                  .attr("height", height + margin.top + margin.bottom)
-                  //.style("background", "lightgrey")
+                  .attr("height", (height + margin.top + margin.bottom)*4)
+                  .style("background", "lightgrey")
                   .append('g')
                   .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
+
+  for (var i = 0; i < years.length; i++) {
+
+    // FILTER DATA BY YEAR
+
+    txData = data.slice();
+
+    txData = txData.filter(function (d) {
+        return yearFormat(dateParser(d.deathDate)) === years[i].toString();
+      }); 
+
     // SCALES
 
-    chart.x = d3.scaleTime()
-                .domain([new Date(year, 0, 1), new Date(year + 1, 0, 1)])
+    x = d3.scaleTime()
+                .domain([new Date(years[i], 0, 1), new Date(years[i] + 1, 0, 1)])
                 .range([0, width])
                 .nice();
-    
+
+    // CREATE YEAR GROUP FOR EACH CHART AND SET POSITION
+
+    yearGroup = chart.svg.append("g")
+     .attr("id", years[i])
+     .attr("transform", "translate(0," + ((height+(margin.top*2)) * (i+2)) + ")")
+
     // AXES
 
     var xAxis = d3.axisBottom()
-                  .scale(chart.x);
+                  .scale(x);
 
-
-    chart.svg.append("g")
+    yearGroup.append("g")
              .attr("class", "axis") 
-             .attr("transform", "translate(0," + (height) + ")")
              .call(xAxis);
 
     // NEST DATA 
 
     var vicsByDate = d3.nest()
-                       .key(function(d) { return d.deathDate})
+                       .key(function(d) { return d.deathDate;})
                        .entries(txData);
 
-console.log(vicsByDate);
-    var dateGroup = chart.svg.selectAll(".day")
-                             .data(vicsByDate)
-                             .enter().append("g")
-                             .attr("transform", "translate(0," + (height - 3) + ")")
-                             .attr("class", "day");
+    // CREATE GROUP FOR EACH DAY
 
+    var dateGroup = yearGroup
+                     .selectAll(".day")
+                     .data(vicsByDate, function (d) { return d.deathDate;})
+                     .enter().append("g")
+                     .attr("class", "day")
+                     .attr("transform","translate(0," + (-cellSize) + ")");
+ 
 
-console.log(dateGroup);
+    // CREATE RECTS FOR VICTIM WITHIN EACH DAY
+
     var victims = dateGroup.selectAll(".victim")
-                           .data(function(d) {return d.values; })
-                           .enter().append("rect")
+                           .data(function(d) {return d.values; }, function(d) { return d.name });
+
+                           victims.enter().append("rect")
                            .attr("class", "victim")
-                           .attr("x", function(d) { return chart.x(dateParser(d.deathDate))} )
+                           .attr("class", function(d) { return d.status})
+                           .attr("x", function(d) { return x(dateParser(d.deathDate))} )
                            .attr("y", function(d,i) { return -(i*cellSize)-(i+1)})
+                           .attr("height", 0)
+                           .attr("width", 0)
                            .attr("rx", .5)
                            .attr("ry", .5)
-                           .attr("height", cellSize)
-                           .attr("width", cellSize)
-                           .attr("class", function(d) { return d.status})
                            .on("mouseover", function(d) {    
                                        d3.select("#vic-name")
                                          .html(d.name);
@@ -110,11 +122,40 @@ console.log(dateGroup);
 
                                        d3.select("#vic-cause")
                                          .html(d.cause);
+
+                                       d3.select("#vic-date")
+                                         .html(d.deathDate);  
+
+                                       d3.select("#vic-summary")
+
+                                       vicRect = d3.select(this);
+                                         
+                                         vicRect
+                                         .attr("height", cellSize+4)
+                                         .attr("width", cellSize+4)
+                                         .attr("x", vicRect.attr("x")*1 - 2)
+                                         .attr("y", vicRect.attr("y")*1 - 2); 
                                       })          
-                            .on("mouseout", function(d) {   
-                                div.transition()    
-                                    .duration(500)    
-                                    .style("opacity", 0);
-                                     }); 
-                              
-  }
+                           .on("mouseout", function(d) {   
+                                  
+                                     d3.select(this)
+                                       
+                                       .attr("height", cellSize)
+                                       .attr("width", cellSize)
+                                       .attr("x", vicRect.attr("x")*1 + 2)
+                                       .attr("y", vicRect.attr("y")*1 + 2);
+                                     })
+                           .transition().duration(1500)
+                                        .delay( function (d, i) { return i*500 })
+                                        .each( function(){
+                                          d3.select(this)
+                                                .attr("height", cellSize)
+                                                .attr("width", cellSize)
+                                        }
+                                              
+                                )
+;
+                           
+                           
+  };                            
+}
